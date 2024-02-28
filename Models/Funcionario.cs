63f9eb5b7c86ba62866e813cb641f22136636807
MukaDeveloper <muka.bertoni@gmail.com>
2024-02-27 22:40:10 -0300

    initial commit

---
+++ Models/Funcionario.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aula02RH.Models.Enuns;
+
+namespace Aula02RH.Models 
+{
+    public class Funcionarios 
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public DateTime DataAdmissao { get; set; }
+        public decimal Salario { get; set; }
+        public TipoFuncionarioEnum TipoFuncionario { get; set; } 
+        public void ReajustarSalario() { Salario = Salario + (Salario * 10/100); }
+        public decimal CalcularDescontoVR(decimal percentual) {
+            decimal desconto = this.Salario * percentual/100;
+            return desconto;
+        }
+        public string ExibirPeriodoExperiencia()
+        {
+            string periodoExperiencia = string.Format("Períodos de Experiencia: {0} até {1}", DataAdmissao, DataAdmissao.AddMonths(3));
+            return periodoExperiencia;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Aula02RH.Models;
+
+namespace Aula02RH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionarios func = new Funcionarios();
+
+            Console.WriteLine("qual o seu nome?");
+
+
+            // func.Id = 10;
+            // func.Nome = "Neymar";
+            // func.Cpf = "12345678910";
+            // func.DataAdmissao = DateTime.Parse("01/01/2000");
+            // func.Salario = 10000.00M;
+            // func.TipoFuncionario = Models.Enuns.TipoFuncionarioEnum.CLT;
+            // string mensagem = func.ExibirPeriodoExperiencia(); 
+            // Console.WriteLine("===============");
+            // Console.WriteLine(mensagem);
+            // Console.WriteLine("=========================");
+        }
+    }
+}
